5d138eebc0eede1eaf3f48002495e8cd3cda56fa
Otávio Pace <otaviopp8@gmail.com>
2017-05-21 12:37:22 -0300

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,10 @@
+import express from 'express'
+import path from 'path'
+
+let app = express()
+
+app.get('/*', (request, response) => {
+    response.sendFile(path.join(__dirname, './index.html'))
+})
+
+app.listen(3000, () => console.log('Running on localhost:3000'))
